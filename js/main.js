a31f91315d5e9664062ace42b45bf5eaa7b7f76d
Euichang Park <euichangpark@gmail.com>
2022-02-14 19:31:24 -0500

    input required

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('.entry-image');
+var $photoPreview = document.querySelector('#preveiw');
+var placeholder = 'images/placeholder-image-square.jpg';
+$photoUrl.addEventListener('input', srcUpdate);
+
+function srcUpdate(event) {
+  $photoPreview.setAttribute('src', placeholder);
+}
